d034e22cefa733ad05222ec11149113629e6c52b
eladzlot <ez.implicit@gmail.com>
2014-11-16 22:08:10 +0200

    feat(manager): create manager API

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -3,6 +3,7 @@ define([], function(){
 	var req = require.config({
 		paths: {
 			questAPI: 'quest/API',
+			managerAPI: 'taskManager/API',
 			underscore: ["//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min","../../bower_components/lodash/dist/lodash.min"],
 			angular: ['//ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min','../../bower_components/angular/angular'],
 			animate: ['//ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular-animate.min', '../../bower_components/angular-animate/angular-animate'],
--- src/managerScript.js
+++ src/managerScript.js
@@ -1,16 +1,19 @@
-define(function(){
-	return {
-		onEnd: function(){console.log('onEnd');},
+define(['managerAPI'], function(Manager){
 
-		sequence: [
-			{
-				type:'quest',
-				scriptUrl: 'questScript1.js'
-			},
-			{
-				type:'quest',
-				scriptUrl: 'questScript2.js'
-			}
-		]
-	};
+	var API = new Manager();
+
+	API.addSettings('onEnd', function(){console.log('onEnd');});
+
+	API.addSequence([
+		{
+			type:'quest',
+			scriptUrl: 'questScript1.js'
+		},
+		{
+			type:'quest',
+			scriptUrl: 'questScript2.js'
+		}
+	]);
+
+	return API.script;
 });
