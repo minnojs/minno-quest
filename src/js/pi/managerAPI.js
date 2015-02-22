e4a1c168b67c6962bf628f65fa6676d709c84af0
eladzlot <eladzlot@gmail.com>
2015-02-22 10:21:04 +0200

    feat(PI): support piTemplate debrief page

--- src/js/pi/managerAPI.js
+++ src/js/pi/managerAPI.js
@@ -3,7 +3,10 @@ define(function(require){
 	var Constructor = require('taskManager/API');
 	var _ = require('underscore');
 	var isDev = /^(localhost|127.0.0.1)/.test(location.host);
-	var messageTemplate = require('text!pi/messageTemplate.html');
+
+	var messageTemplate = require('text!pi/messageTemplate.jst');
+	var messageTemplateDebrief = require('text!pi/messageTemplateDebrief.jst');
+	var messageTemplatePanel = require('text!pi/messageTemplatePanel.jst');
 
 	/**
 	 * Constructor for PIPlayer script creator
@@ -32,8 +35,9 @@ define(function(require){
 	 * @return {Promise}            Resolved when server responds
 	 */
 	function onPreTask(currentTask, $http, $rootScope){
+
 		var global = $rootScope.global;
-		var settings;
+		var settings, context;
 		var data = {taskName: currentTask.name || 'namelessTask', taskNumber: currentTask.$meta.number};
 
 		// add logging meta
@@ -44,12 +48,24 @@ define(function(require){
 		}
 
 		if (currentTask.type == 'message' && currentTask.piTemplate){
-			currentTask.$template = _.template(messageTemplate,{
+			context = {
 				content: currentTask.$template,
 				global: global,
 				current: global.current,
 				task: currentTask
-			});
+			};
+
+			if (currentTask.piTemplate == 'debrief'){
+				_.extend(context,{
+					showFeedback: _.bind(showFeedback,null,global),
+					showPanel: showPanel
+				});
+
+				currentTask.$template = _.template(messageTemplateDebrief,context); // insert into meta template
+				currentTask.$template = _.template(currentTask.$template,context); // render secondary template with extended context
+			} else {
+				currentTask.$template = _.template(messageTemplate,context); // insert into meta template
+			}
 		}
 
 		// set last task flag
@@ -59,4 +75,44 @@ define(function(require){
 
 		return isDev ? true : $http.post('/implicit/PiManager', data);
 	}
+
+	function showPanel(content, header, footer){
+		return _.template(messageTemplatePanel, {
+			content: content,
+			header: header,
+			footer: footer
+		});
+	}
+
+	function showFeedback(global, options){
+		_.isPlainObject(options) || (options = {});
+
+		_.defaults(options,{
+			pre: '<p>',
+			post: '</p>',
+			wrap: true,
+			header: '',
+			noFeedback: '<p>No feedback was found</p>',
+			property: 'feedback',
+			exclude: []
+		});
+
+		var feedback = _(global)
+			.filter(function(task,taskName){
+				if (!_.isArray(options.exclude)) { throw Error('Exclude must be an array'); }
+				// make sure task is an object
+				// make sure feedback is defined
+				// make sure this task is not excluded
+				return _.isPlainObject(task) && !_.isUndefined(task[options.property]) && (_.indexOf(options.exclude,taskName) === -1);
+			})
+			.mapValues(function(task){
+				return options.pre + task[options.property] + options.post;
+			})
+			.reduce(function(result, feedback){
+				return result + feedback;
+			},'') || options.noFeedback;
+
+		return options.wrap ? showPanel(feedback, options.header) : feedback;
+	}
 });
+
