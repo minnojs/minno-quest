a112a09236847242d1d163278950a6f452d50e7d
eladzlot <eladzlot@gmail.com>
2015-05-22 12:41:19 +0300

    fix(safari): fixed unexplained problem on IOS8. https://github.com/angular/angular.js/issues/9128

--- src/js/app.js
+++ src/js/app.js
@@ -32,5 +32,72 @@ define(function (require) {
 		}
 	}]);
 
+	// monkey patch an IOS8 problem
+	// https://github.com/angular/angular.js/issues/9128
+	app.config(['$provide', function($provide) {
+
+	  // Minification-safe hack.
+	  var $$watchers = '$$watchers',
+	      $$nextSibling = '$$nextSibling',
+	      $$childHead = '$$childHead',
+	      $$childTail = '$$childTail',
+	      $$listeners = '$$listeners',
+	      $$listenerCount = '$$listenerCount',
+	      $id = '$id',
+	      $$childScopeClass = '$$childScopeClass',
+	      $parent = '$parent',
+	      $$prevSibling = '$$prevSibling';
+
+	  $provide.decorator('$rootScope', ['$delegate', function($rootScope) {
+	    var proto = Object.getPrototypeOf($rootScope);
+
+	    function nextUid () {
+	      return ++$rootScope.$id;
+	    }
+
+	    proto.$new = function(isolate) {
+	      var child;
+
+	      if (isolate) {
+	        child = new proto.constructor();
+	        child.$root = this.$root;
+	        // ensure that there is just one async queue per $rootScope and its children
+	        child.$$asyncQueue = this.$$asyncQueue;
+	        child.$$postDigestQueue = this.$$postDigestQueue;
+	      } else {
+	        // Only create a child scope class if somebody asks for one,
+	        // but cache it to allow the VM to optimize lookups.
+	        if (!this.$$childScopeClass) {
+	          this.$$childScopeClass = function() {
+	            this[$$watchers] = this[$$nextSibling] =
+	                this[$$childHead] = this[$$childTail] = null;
+	            this[$$listeners] = {};
+	            this[$$listenerCount] = {};
+	            this[$id] = nextUid();
+	            this[$$childScopeClass] = null;
+	          };
+	          this.$$childScopeClass.prototype = this;
+	        }
+	        child = new this.$$childScopeClass();
+	      }
+	      child['this'] = child;
+	      child[$parent] = this;
+	      child[$$prevSibling] = this.$$childTail;
+	      if (this.$$childHead) {
+	        this.$$childTail.$$nextSibling = child;
+	        this.$$childTail = child;
+	      } else {
+	        this.$$childHead = this.$$childTail = child;
+	      }
+	      return child;
+	    };
+
+	    $rootScope.$new = proto.$new;
+	    return $rootScope;
+	  }]);
+
+	}]);
+
+
 	return app;
 });
