48816d681f380b0596372dd1447a253ab47a1591
eladzlot <eladzlot@gmail.com>
2015-05-22 10:49:43 +0300

    fix(bootstrap): fixed race condition between domready and jqlite.ready that caused problems with startup

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -9,9 +9,18 @@ define(['./config'], function(){
 		]
 	});
 
+	// resume boot strap
+	// http://stackoverflow.com/a/25770449/1400366
+	// The angular.js detects dom ready via JQLite. When the document loads, it triggers a method which defines the resumeBootstrap method.
+	// The problem is that you cannot enforce processing order between domready and JQLite ready,
+	// if domready already fulfills your dependency before the method gets defined, you are in trouble.
+	// The workaround is to enforce javascript to execute all event handlers before your resumeBootstrap invocation.
+	// I've used the setTimeout method to do so, e.g.: setTimeout(angular.resumeBootstrap, 0).
 	require(['angular','app'], function(angular, app) {
 		angular.element().ready(function() {
-			angular.resumeBootstrap([app.name]);
+			setTimeout(function(){
+				angular.resumeBootstrap([app.name]);
+			},0);
 		});
 	});
 });
