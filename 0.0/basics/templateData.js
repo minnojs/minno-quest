ae6631a20ca0534faeda873cf3a87435fbe89680
eladzlot <eladzlot@gmail.com>
2015-07-05 12:52:01 +0300

    fix(template): remove html that was inserted by accident

--- 0.0/basics/templateData.js
+++ 0.0/basics/templateData.js
@@ -1,6 +1,6 @@
 // You can use data set within the objects you are using from within templates.
 // Check the documentation for the task you are using to know how the data object is called from within the template
-// (the standard structure for the name is <object type>Data, so that tasks become tasksData).
+// (the standard structure for the name is {{object type}}Data, so that tasks become tasksData).
 define(['managerAPI'], function(Manager){
 
   var API = new Manager();
--- src/0.0/basics/templateMeta.js
+++ src/0.0/basics/templateMeta.js
@@ -1,6 +1,6 @@
 // Each object has a special object called "meta" that has information regarding the relative location of the object within the sequence.
 // Check the documentation for the task you are using to know how the meta object is called from within the template.
-// (the standard structure for the name is <object type>Meta, so that tasks meta become tasksMeta).
+// (the standard structure for the name is {{object type}}Meta, so that tasks meta become tasksMeta).
 //
 // The meta objects has two properties `number` and `outOf`.
 
