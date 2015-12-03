449d0fafa6cf84e342ab3a24161ca9f30f417ade
eladzlot <eladzlot@gmail.com>
2015-12-03 10:08:05 +0200

    chore(upgrade): v0.0.68

---
+++ 0.0/dist/validator/managerValidator.js
@@ -0,0 +1,18 @@
+define(function(require){
+
+	var messages = require('./messages');
+	var managerParser = require('./parser').managerElements;
+
+	function managerValidator(script){
+		var errors = [];
+		var elements = managerParser(script);
+
+		errors.push({type:'Settings', errors:[]});
+		errors.push({type:'Tasks', errors:[]});
+
+		return errors;
+	}
+
+	return managerValidator;
+
+});
