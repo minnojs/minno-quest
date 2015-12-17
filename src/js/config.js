938e31ad82a08a4d9d12dc128067843e934e92c3
eladzlot <eladzlot@gmail.com>
2015-12-17 09:23:31 +0200

    feat(bootstrap): allow manual bootstrap (maybe solve the missing resumeBootstrap bug?)

--- src/js/config.js
+++ src/js/config.js
@@ -8,9 +8,6 @@ define(function(){
 	window.console.log || (window.console.log = noop);
 	window.console.warn || (window.console.warn = noop);
 
-	//http://code.angularjs.org/1.2.1/docs/guide/bootstrap#overview_deferred-bootstrap
-	window.name = 'NG_DEFER_BOOTSTRAP!';
-
 	require.config({
 		// in order to catch IE errors
 		enforceDefine: true,
--- src/manager.jsp
+++ src/manager.jsp
@@ -69,7 +69,7 @@ String getBase = getProtocol+"://"+getDomain;
 		</style>
 	</head>
 
-	<body ng-app>
+	<body id="pi-app">
 		<!--[if lt IE 7]>
 			<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 		<![endif]-->
