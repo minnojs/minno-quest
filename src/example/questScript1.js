0cfc5d519ce18613a85e5fe9e7c43804b64b2cd2
eladzlot <ez.implicit@gmail.com>
2016-07-28 17:52:28 +0300

    feat(rank): problems with the select option

--- src/example/questScript1.js
+++ src/example/questScript1.js
@@ -26,6 +26,16 @@ define(['questAPI'], function(Quest){
 	});
 
 	API.addSequence([
+        {
+            questions: [
+                {
+                    type: 'rank',
+                    stem: 'rank quest',
+                    name: 'mart',
+                    list: [1,2,3,4,5,6]
+                }
+            ]
+        },
 {
     remix: true,
     mixer:'branch',
