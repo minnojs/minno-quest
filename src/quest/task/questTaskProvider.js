1ddfd8a9cd312e0c46312c8c157e475eb44d7e57
eladzlot <ez.implicit@gmail.com>
2017-12-14 10:43:20 +0200

    feat(logger): inject global. and taskName into metaData

--- src/quest/task/questTaskProvider.js
+++ src/quest/task/questTaskProvider.js
@@ -7,12 +7,13 @@ function TaskProvider($q, Database, Logger, QuestSequence, parse, dfltQuestLogge
     function Task(script){
         var self = this;
         var settings = script.settings || {};
+        var global = $rootScope.global;
 
         // save script for later use...
         this.script = script;
         this.db = new Database();
         this.$logSource = stream();
-        this.logger = new Logger(this.$logSource, settings.logger || {}, dfltQuestLogger);
+        this.logger = new Logger(this.$logSource, composeLoggerSettings(script, global), dfltQuestLogger);
         this.q = $q.defer();
 
         if (!_.isArray(script.sequence)) throw new Error('Task: no sequence was defined');
@@ -24,13 +25,21 @@ function TaskProvider($q, Database, Logger, QuestSequence, parse, dfltQuestLogge
                 // check if there are unlogged questions and log them
                 _.each($rootScope.current.questions, function(quest){
                     if(quest.$logged){ return true; }
-                    self.log(quest, {}, $rootScope.global);
+                    self.log(quest, {}, global);
                     quest.$logged = true;
                 });
                 self.logger.end(true);
             })['finally'](settings.onEnd || angular.noop); // end only after logging has finished (regardless of success)
 
         parse(script, this.db);
+    }
+
+    // create metaDeta to add to post
+    function composeLoggerSettings(script, global){
+        var loggerSettings = _.assign({}, _.get(script, 'settings.logger'));
+        var metaData = _.assign({taskName:script.name}, global.$meta, loggerSettings.metaData);
+        loggerSettings.metaData = metaData;
+        return loggerSettings;
     }
 
     _.extend(Task.prototype, {
