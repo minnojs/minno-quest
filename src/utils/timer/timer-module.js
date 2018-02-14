083ccf86838fae4ffef52384e494f1b63d3e722c
eladzlot <ez.implicit@gmail.com>
2018-02-14 11:17:38 +0200

    fix(modal): now works without the root #pi-app

--- src/utils/timer/timer-module.js
+++ src/utils/timer/timer-module.js
@@ -1,12 +1,12 @@
-
 import angular from 'angular';
+import timerStopper from './timerStopper';
+import timerDirective from './timerDirective';
+import timerNow from './timerNow';
+
 var module = angular.module('timer',[]);
 
 module.service('timerStopper', timerStopper);
-import timerStopper from './timerStopper';
 module.directive('piTimer', timerDirective);
-import timerDirective from './timerDirective';
 module.value('timerNow', timerNow);
-import timerNow from './timerNow';
 
 export default module;
