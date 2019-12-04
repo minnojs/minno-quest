96d6a82a1848295732e2d8a689945d729ffba8f4
eladzlot <ez.implicit@gmail.com>
2019-12-04 20:31:31 +0200

    fix(time): updated time API, time now waits for 'onDone'

--- src/taskManager/task/tasks/timeTask.js
+++ src/taskManager/task/tasks/timeTask.js
@@ -15,15 +15,16 @@ function activateTime(done, $canvas, task, script, piConsole, logger){
     $el = $canvas.contents();
 
     pipSink = time($el[0], script);
-    pipSink.onEnd(done);
+    pipSink.promise.then(done);
 
     pipSink.$messages.map(piConsole);
     pipSink.$logs.map(log);
     pipSink.$logs.end.map(log.end);
 
-    return function destroyPIP(){
+    return function destroy(){
         log.end(true);
         $el.remove();
         pipSink.end();
+        return pipSink.promise;
     };
 }
