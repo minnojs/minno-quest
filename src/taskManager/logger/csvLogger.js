8d6755c50516704bdb96c839b50c5e02d30d575c
eladzlot <ez.implicit@gmail.com>
2021-04-26 12:08:51 +0300

    feat(logger): csvLogger supports singleTask logging

--- src/taskManager/logger/csvLogger.js
+++ src/taskManager/logger/csvLogger.js
@@ -7,7 +7,7 @@ export default {
         var logs = ctx.csv || (ctx.csv = []);
         logs.push(_.assign({taskName:name}, row));
     },
-    onEnd: function(name, settings, ctx){ if (name == 'manager') return ctx.csv; },
+    onEnd: function(name, settings, ctx){ if (name == 'manager' || settings.singleTask) return ctx.csv; },
     serialize: function(name, logs){ return toCsv(createMatrix(logs)); },
     send: function(name, serialized, settings,ctx){ 
         var url = _.isString(settings.postCsv) ? settings.postCsv : settings.url;
